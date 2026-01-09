eef773557946dfd8419fc5e45973fa49a81bb962
jabdalexmei <jabdalexmei@gmail.com>
2026-01-09 14:21:25 +0300

    Show badges only when present

--- src/features/Vault/components/DataCards/DataCards.tsx
+++ src/features/Vault/components/DataCards/DataCards.tsx
@@ -827,7 +827,7 @@ export function DataCards({
               >
                 <div className="datacard-top">
                   <div className="datacard-title">{displayTitleText}</div>
-                  {!isUntitledPlaceholder && (
+                  {(card.hasTotp || isFavorite) && (
                     <div className="datacard-badges">
                       {card.hasTotp && <span className="pill">{t('twoFactor.pill')}</span>}
                       {isFavorite && <span className="pill datacard-favorite">{t('label.favorite')}</span>}
